583d10400de3b5ab939b1df1b37f1151444a7589
geekstar8846 <geekstar@sina.com>
2016-04-25 16:07:43 +0800

    add IResultTable

--- ac/src/main/java/com/geekstar/batch/ac/Interface/IResultTable.java
+++ ac/src/main/java/com/geekstar/batch/ac/Interface/IResultTable.java
@@ -1,5 +1,14 @@
 package com.geekstar.batch.ac.Interface;
 
-public interface IResultTable {
+import java.util.List;
 
+import com.geekstar.batch.ac.Bean.ResultBean;
+
+public interface IResultTable {
+	//保存一条对账结果到内存
+	public void addResultBean(ResultBean bean);
+	public List<ResultBean> getResultBeanList();
+	
+	//转存当前“账务日期”的所有对账结果到database
+	public void saveToDB(String date);
 }
